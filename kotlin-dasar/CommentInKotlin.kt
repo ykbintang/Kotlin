197d9acdc8acaf5dc406154cc86103a0116778db
Yayes Kasnanda Bintang <ykbintang07@gmail.com>
2023-03-13 14:50:46 +0700

    update files

--- kotlin-dasar/CommentInKotlin.kt
+++ kotlin-dasar/CommentInKotlin.kt
@@ -1,5 +1,13 @@
 package com.dicoding.kotlin
 
+// ## Comment In Kotlin
+/*
+    => Komentar terbaik pada kode adalah kode itu sendiri
+    => Saat membuat kode, kita perlu membuat kode semudah mungkin untuk dibaca
+    => Namun kadang juga kita butuh menambahkan komentar di kode Kotlin kita
+ */
+
+// #1 Contoh multiple line comment
 /**
  *  This
  *  is
@@ -10,4 +18,9 @@ package com.dicoding.kotlin
  *  kotlin
  */
 
-// This is single line comment in kotlin
+fun main() {
+
+    // #2 Contoh single line comment
+    // This is single line comment in kotlin
+
+}
